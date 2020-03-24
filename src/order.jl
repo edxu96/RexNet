fab105cb2f240883975fbf91f3e0ccec4fd16042
edxu96 <edxu96@outlook.com>
2020-03-24 17:33:42 +0100

    update

--- src/order.jl
+++ src/order.jl
@@ -1,25 +1,44 @@
 module Order
 
-```
-Once defined, `OrderRaw` cannot be changed.
 
-the link to `sizes` is established in LOB-3D.
-If the seat is expired or cancelled, the `OrderRaw` remains unchanged.
 ```
-struct Order
+    LimitOrderSet(time::real, client::Int64)
+```
+struct LimitOrderSet
+  ## Onced defined, following three variables are immutable.
   time::real
   client::Int64
   seats::DataFrame
-  sizes::DataFrame  # history for changes of sizes
 
-  ```
-  Initialize the order
-  ```
-  function Order(args)
-    body
+  function LimitOrderSet(time::real, client::Int64)
+    ## Initialize the data frame for seats
+    seats = DataFrame(target = Int64[], price = Int64[],
+      sizes = MutableLinkedList{Int64}[])
 
-    new(time, client, seats, sizes_init)
+    new(time, client, seats)
   end
+end
+
+
+```
+    quote_size(seats::DataFrame, target::Int64, price::Int64)
+
+Given seat position, quote the size of the limit order.
+```
+function quote_size(seats::DataFrame, target::Int64, price::Int64)
+  df = filter(row -> (row[:target] == target) & (row[:price] == price), seats)
+
+  if nrow(df) == 0
+    println("There is no seat for target $(target) at price $(price).")
+  elseif nrow(df) > 1
+    println("There is duplicate seats for target $(target) at price $(price).
+      Error!")
+  else
+    mll = df.sizes[1]
+    size = sum(collect(mll))
+  end
+
+  return size
 end
 
 end
--- src/rex.jl
+++ src/rex.jl
@@ -9,18 +9,20 @@ struct NodePrice
   price::Int64
   sq::MutableLinkedList{Order}  # [seat queue]
 
-  function NodePrice(price)
+  function NodePrice(price::Int64)
     new(price, MutableLinkedList{Order}())
   end
 end
 
+
+remove_seat!(sq::MutableLinkedList{LimitOrderSet}, )
 
 ```
 For some seat in some set of limit orders, find the corresponding seat queue
 in Rex and push the order into the queue. Taht is, the reference of the order
 is add at the end of the seat queue.
 ```
-function push_seat!(sq::MutableLinkedList{Order}, odr::Order)
+function push_seat!(sq::MutableLinkedList{LimitOrderSet}, odr::Order)
   push!(sq, odr)
 end
 
@@ -47,13 +49,14 @@ end
     NodeTarget(target, price_bid, price_ask)
 
 The initial spread is the result of market clearing in the opening auction.
+0 is used as the default value for bid prices and ask prices.
 ```
 mutable struct NodeTarget
   target::Int64  # [index of target unit]
-  bid::NodePrice
-  ask::NodePrice
+  bid::NodePrice  # [price nodes on the bid side]
+  ask::NodePrice  # [price nodes on the ask side]
 
-  function NodeTarget(target, price_bid, price_ask)
+  function NodeTarget(target::Int64, price_bid::Int64=0, price_ask::Int64=0)
     new(target, price_bid, price_ask)
   end
 end
@@ -63,18 +66,21 @@ end
     Rex(lot_size, tick_size, num_forward)
 
 Reservation Exchange
+
+Three resolution parameters of LOB-3D:
+- lot_size: [size of lot] the smallest amount of the asset that can be traded
+  within it.
+- tick_size: [size of tick] the smallest permissible price interval between
+  different orders within it.
+- num_forward: [number of forward target units]
 ```
 struct Rex
-  ## Three resolution parameters of LOB-3D.
-  lot_size::Float64  # [size of lot] the smallest amount of the asset that
-  # can be traded within it.
-  tick_size::Float64  # [size of tick] the smallest permissible price interval
-  # between different orders within it.
-  num_forward::Int64  # [number of forward target units]
-
+  lot_size::Float64
+  tick_size::Float64
+  num_forward::Int64
   book::MutableLinkedList{NodeTarget}
 
-  function Rex(lot_size, tick_size, num_forward)
+  function Rex(lot_size::Float64, tick_size::Float64, num_forward::Int64)
     ## Initialize the set of LOB3D with dummy target nodes.
     book = MutableLinkedList{NodeTarget}()
     for i = 1: num_forward
